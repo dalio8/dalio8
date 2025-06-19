91d56a23f3b4574b27b5fda795de0a28c70ed1d7
dalio8 <dalio8@hotmail.com>
2025-06-18 22:56:04 -0300

    Initialize Vite React setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-4 text-center">
+      <h1 className="text-2xl font-bold">Hello Vite + React!</h1>
+    </div>
+  );
+}
+
+export default App;
